4d4e6d69c0e25e261e84d496bc53fa3cacebc979
LiPtP0000 <13998189046@163.com>
2024-09-23 17:48:41 +0800

    数学公式支持

--- example/index.js
+++ example/index.js
@@ -1,37 +1,45 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// 引入 highlight.js
+const hljs = require("highlight.js");
+const fs = require("fs");
+const path = require("path");
 
 // 语法高亮
-//   引入 highlight.js 并配置到 markdown-it
-const hljs = require('highlight.js')
 const highlight = (str, lang) => {
-  let code = md.utils.escapeHtml(str)
+  let code = md.utils.escapeHtml(str);
   if (lang && hljs.getLanguage(lang)) {
-    code = hljs.highlight(lang, str, true).value
+    code = hljs.highlight(lang, str, true).value;
   }
-  return `<pre class="hljs"><code>${code}</code></pre>`
-}
-const md = require('markdown-it')({ highlight })
-const fs = require('fs')
-const path = require('path')
+  return `<pre class="hljs"><code>${code}</code></pre>`;
+};
+const md = require("markdown-it")({ highlight });
 
 // 配置模版引擎
-app.engine('html', require('./template-engine'))
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'html')
+app.engine("html", require("./template-engine"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "html");
 
 // 静态资源
-app.use('/public', express.static(path.join(__dirname, 'public')))
+app.use("/public", express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-  fs.readFile('public/posts/demo.md', (err, content) => {
-    if (err) return res.status(500).send('服务器错误 ^.^')
-    content = content.toString()
-    res.render('template', { content: md.render(content) })
-  })
-})
+// 读取并解析 Markdown 文件
+app.get("/", (req, res) => {
+  fs.readFile("public/posts/ch1.md", (err, content) => {
+    if (err) return res.status(500).send("服务器错误 ^.^");
+    content = content.toString();
+
+    // 渲染 Markdown 内容
+    const renderedContent = md.render(content);
+
+    // 渲染页面
+    res.render("template", { content: renderedContent });
+    console.log(renderedContent);
+  });
+});
 
 app.listen(port, () => {
-  console.log(`app listening on port http://localhost:${port}`)
-})
+  console.log(`app listening on port http://localhost:${port}`);
+});
---
+++ example/public/posts/overview.md
@@ -0,0 +1,22 @@
+# Digital Signal Processing
+
+## Overview
+
+1. Sampling and Reconstruction
+2. DFT/FFT
+3. Filter
+4. IIR Filter
+5. FIR Filter
+6. Implementation
+
+目前正在研究压缩感知。稀疏性、稀疏重建、欠定方程求解等等。
+
+## Signal and Continuous Signal
+
+- signal
+  - continuous signal
+    - continus time signal：时间连续、幅度不一定
+    - Analog signal：时间连续、幅度连续
+  - discrete signal
+    - digital signal：时间幅度均不连续
+    - discrete time signal: 时间离散、幅度连续
